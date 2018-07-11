071e187ae7f40d7f5c07ec8c837ce2385413fd3c
LysAMa <ladouceurlyse44@gmail.com>
2018-07-10 20:51:26 -0400

    splash

---
+++ app/src/main/java/com/example/root/moviemovie/splash.java
@@ -0,0 +1,23 @@
+package com.example.root.moviemovie;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splash extends AppCompatActivity {
+    public static int  spa = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splash.this, MainActivity.class));
+                finish();
+            }
+        }, spa);
+    }
+}
